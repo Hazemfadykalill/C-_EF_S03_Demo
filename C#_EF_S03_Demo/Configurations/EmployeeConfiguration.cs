d13387d6938a1e1bf0cd25bd0e6a9a7cc43dc389
Hazem Fady <hazemfady123@gmail.com>
2024-08-28 23:12:36 +0300

    Mapping Classes To Tables (Employee - Department)

---
+++ C#_EF_S03_Demo/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using C__EF_S03_Demo.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__EF_S03_Demo.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            // Primary Key
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).UseIdentityColumn(1,3);    
+
+
+            // Properties
+            builder.Property(e => e.Id)
+                .IsRequired()  // Make sure the Id is required (default for primary keys)
+                .ValueGeneratedOnAdd(); // Automatically generates value on adding a new record
+
+            builder.Property(e => e.Name)
+                .IsRequired() // Name is required
+                .HasMaxLength(100); // Sets a max length of 100 characters
+
+            builder.Property(e => e.Salary)
+                .IsRequired() // Salary is required
+                .HasColumnType("decimal(18,2)"); // Configure as decimal with 2 decimal places for better precision
+
+            builder.Property(e => e.Address)
+                .HasMaxLength(200); // Sets a max length of 200 characters
+
+            builder.Property(e => e.Dept_Id)
+                .IsRequired(); // Department ID is required
+
+            // Relationships
+            builder.HasOne(e => e.Department)
+                .WithMany(d => d.Employees) // Assuming Department class has an Employees collection
+                .HasForeignKey(e => e.Dept_Id);
+        }
+    }
+}
